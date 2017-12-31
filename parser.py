f83c555e08b4e4d9ae0ed613e531039a487fadf2
Lucasfrota <frota_lv@hotmail.com>
2017-12-30 23:14:39 -0400

    inicial lang features

---
+++ parser.py
@@ -0,0 +1,121 @@
+from sys import *
+import ply.yacc as yacc
+from tolkenizer import tokens
+
+precedence = (
+    ('left', 'PLUS', 'MINUS'),
+    ('left', 'TIMES', 'DIVIDE'),
+    ('right', 'UMINUS'),
+)
+
+variables = {}
+
+def p_print_expression(p):
+    'statement : PRINT expression'
+    print p[2]
+
+def p_print_string(p):
+    'statement : PRINT QUOTATION ID QUOTATION'
+    print p[3]
+
+def p_if(p):
+    'statement : IF expression '
+    print p[2]
+
+def p_statement_assign(p):
+    'statement : ID RECEIVE expression NEWLINE'
+    variables[p[1]] = p[3]
+
+def p_string(p):
+    'statement : ID RECEIVE QUOTATION ID QUOTATION NEWLINE'
+    variables[p[1]] = p[4]
+
+def p_statement_expr(p):
+    'statement : expression'
+    p[0] = p[1]
+
+def p_statement_paren(p):
+    'statement : LPAREN expression RPAREN'
+    p[0] = p[2]
+
+def p_boolean_expression(p):
+    '''expression : expression LT expression
+                    | expression LE expression
+                    | expression GT expression
+                    | expression GE expression'''
+    if p[2] == '<':
+        p[0] = p[1] < p[3]
+    elif p[2] == '<=':
+        p[0] = p[1] <= p[3]
+    elif p[2] == '>':
+        p[0] = p[1] > p[3]
+    elif p[2] == '>=':
+        p[0] = p[1] >= p[3]
+
+def p_expression_binop(p):
+    '''expression : expression PLUS expression
+                  | expression MINUS expression
+                  | expression TIMES expression
+                  | expression DIVIDE expression'''
+    if p[2] == '+':
+        p[0] = p[1] + p[3]
+    elif p[2] == '-':
+        p[0] = p[1] - p[3]
+    elif p[2] == '*':
+        p[0] = p[1] * p[3]
+    elif p[2] == '/':
+        p[0] = p[1] / p[3]
+
+def p_expression_uminus(p):
+    "expression : '-' expression %prec UMINUS"
+    p[0] = -p[2]
+
+def p_expression_group(p):
+    "expression : '(' expression ')'"
+    p[0] = p[2]
+
+def p_expression_number(p):
+    '''expression : FLOAT
+                  | INTEGER'''
+    p[0] = p[1]
+
+def p_expression_id(p):
+    "expression : ID"
+    try:
+        p[0] = variables[p[1]]
+    except LookupError:
+        print("Undefined variable '%s'" % p[1])
+        p[0] = 0
+
+def p_error(p):
+    if p:
+        print("Syntax error at '%s'" % p.value)
+    else:
+        print("Syntax error at EOF")
+
+parser = yacc.yacc()
+
+def get_parser():
+    return parser
+
+def open_file(file_name):
+    data = open(file_name, "r").read()
+    return data
+
+if __name__ == '__main__':
+
+    try:
+
+        data = open_file(argv[1])
+        parser.parse(data)
+
+    except:
+
+        while True:
+           try:
+               s = raw_input('lazy_cat> ')
+           except EOFError:
+               break
+           if not s:
+               continue
+           parser.parse(s)
---
+++ tolkenizer.py
@@ -0,0 +1,120 @@
+import ply.lex as lex
+
+tokens = [
+   'ID',
+   'INTEGER',
+   'FLOAT',
+   'PLUS',
+   'MINUS',
+   'TIMES',
+   'DIVIDE',
+   'LPAREN',
+   'RPAREN',
+   'SQUARED',
+   'QUOTATION',
+   'RECEIVE',
+   'NEWLINE',
+]
+
+boolean_operators = [
+    'LT',
+    'LE',
+    'GT',
+    'GE',
+]
+
+reserved = {
+   'print' : 'PRINT',
+   'if' : 'IF',
+   'for' : 'FOR',
+   'else' : 'ELSE',
+   'true' : 'TRUE',
+   'false' : 'FALSE',
+   'none' : 'NONE',
+   'or' : 'OR',
+   'and' : 'AND',
+   'not' : 'NOT',
+   'return' : 'RETURN',
+}
+
+literals = [ '{', '}']
+
+tokens = tokens + boolean_operators + list(reserved.values())
+
+t_PLUS      = r'\+'
+t_MINUS     = r'-'
+t_TIMES     = r'\*'
+t_DIVIDE    = r'/'
+t_LPAREN    = r'\('
+t_RPAREN    = r'\)'
+t_QUOTATION = r'\"'
+t_RECEIVE   = r'\='
+
+t_LT        = r'<'
+t_LE        = r'<='
+t_GT        = r'>'
+t_GE        = r'>='
+
+def t_INTEGER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_FLOAT(t):
+    r'((\d*\.\d+)(E[\+-]?\d+)?|([1-9]\d*E[\+-]?\d+))'
+    t.value = float(t.value)
+    return t
+
+def t_NEWLINE(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+t_ignore  = ' \t'
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'ID')    # Check for reserved words
+    return t
+
+def t_COMMENT(t):
+    r'\//.*'
+    pass
+
+def t_SQUARED(t):
+    r'\*\*'
+    return t
+
+def find_column(input,token):
+    last_cr = input.rfind('\n',0,token.lexpos)
+    if last_cr < 0:
+	       last_cr = 0
+    column = (token.lexpos - last_cr) + 1
+    return column
+
+'''
+def t_eof(t):
+    # Get more input (Example)
+    more = raw_input('... ')
+    if more:
+        self.lexer.input(more)
+        return self.lexer.token()
+    return N ""
+'''
+
+lexer = lex.lex()
+
+data = '''
+while 3 = ** 4.4 "" {
+    1
+}
+'''
+
+lexer.input(data)
+
+if __name__ == '__main__':
+    for tok in lexer:
+        print(tok)
